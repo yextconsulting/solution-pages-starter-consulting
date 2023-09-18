4637015e06b994b80ed215828b45d4b6e56a7b9b
yshi <yshi@yext.com>
2023-09-18 11:01:14 -0400

    WIP: RPS site gen

--- default/resources/platform/plugin/slugs/mod.ts
+++ default/resources/platform/plugin/slugs/mod.ts
@@ -1,12 +1,20 @@
-import { createSlugManager } from "https://deno.land/x/yextpages@plugins@1.0.0-beta.5/mod.ts";
+import { createSlugManager } from "https://deno.land/x/yextpages@plugins@1.0.0-beta.3/mod.ts";
 
 declare const API_KEY: string;
 
+const countriesWithRegion = [
+	"AE", "AU", "BB", "CA", "CH", "DM", "HK", "IE", "IN", "KN", "KY", "LC", "US"
+]
+
 export const {webhook, connector} = createSlugManager({
 	apiKey: API_KEY,
-	slugFormat: "[[localeCode]]/[[address.region]]/[[address.city]]/[[address.line1]]",
-	slugFormatLocaleOverrides: {
-		"en": "[[address.region]]/[[address.city]]/[[address.line1]]"
+	slugFormat: "[[localeCode]]/[[countryCode]]/[[address.region]]/[[address.city]]/[[address.line1]]/[[entityId]]",
+	slugGeneratorFn: (lang, profile) => {
+		if (profile.address && profile.address.countryCode && countriesWithRegion.indexOf(profile.address.countryCode) !== -1) {
+			return `[[localeCode]]/${profile.address.countryCode.toLowerCase()}/[[address.region]]/[[address.city]]/[[address.line1]]/[[entityId]]`
+		} else if (profile.address && profile.address.countryCode){
+			return `[[localeCode]]/${profile.address.countryCode.toLowerCase()}/[[address.city]]/[[address.line1]]/[[entityId]]`
+		}
 	},
-	entityTypes: ["location"],
+	slugGeneratorFnFields: ['address'],
 });
